2077b3f755031726bc871326009c50d73059c5d6
viruzvz <viruzvz@gmail.com>
2021-10-30 00:58:25 -0300

    atualizações

--- src/index.js
+++ src/index.js
@@ -7,7 +7,16 @@ window.$ = window.jQuery = $
 
 !(function($) {
   "use strict"
+  
+// contact form 7 + bootstrap
 
+$(document).ready(function () {
+  // $('#wpcf7-f122-o1').addClass('container')
+  $('.wpcf7-form').addClass('row')
+  $('.c7-md-6').parent().parent().parent().addClass('col-md-6')
+  $('.c7-md-12').parent().parent().parent().addClass('col-md-12')
+  $('.wpcf7-submit').parent().addClass('col-md-12')
+})
 
 
 })(jQuery)
